c98ed3dd6f991ab9a9d1349e253bd917855591f7
Nick Paisley <2ep303@gmail.com>
2019-04-06 14:40:06 -0500

    done except for h1

--- main.py
+++ main.py
@@ -11,44 +11,47 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
      <form method="post">
         <label>Rotate By:</label>
         <input type="text" name="rot" value="0"/>
-        <input type="textarea" name="textarea"/>
-        <input type="submit"/>
+        <textarea name="text">{0}</textarea>
+        <input type="submit" value="submit"/>
 
 
     </body>
 </html>
 """
 
+@app.route("/")
+def index():
+    return form.format("")
+
+
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = int(request.form['rot'])
-    msg = request.form['textarea']
-    encrypted = "<h1>" + rotate_string(msg, rot) + "</h1>"
+    msg = request.form['text']
+    encrypted = rotate_string(msg, rot)
     
-    return encrypted
+    return form.format(encrypted)
 
 
-@app.route("/")
-def index():
-    return form
+
 
 app.run()
